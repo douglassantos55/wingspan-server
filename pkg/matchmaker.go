edd41d6be1695628afbacf9400d24555041f5088
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-10-20 22:34:24 -0300

    refactor: matchmaker

--- pkg/matchmaker.go
+++ pkg/matchmaker.go
@@ -1,10 +1,6 @@
 package pkg
 
-import (
-	"container/list"
-
-	"github.com/google/uuid"
-)
+import "github.com/google/uuid"
 
 type Match struct {
 	ID        uuid.UUID
@@ -34,55 +30,13 @@ func (m *Match) Accept(socket *Socket) bool {
 }
 
 type Matchmaker struct {
-	queue    *list.List
-	players  int
-	matches  map[string]*Match
-	elements map[*Socket]*list.Element
+	matches map[string]*Match
 }
 
 func NewMatchmaker(numPlayers int) *Matchmaker {
 	return &Matchmaker{
-		queue:    list.New(),
-		players:  numPlayers,
-		matches:  make(map[string]*Match),
-		elements: make(map[*Socket]*list.Element),
+		matches: make(map[string]*Match),
 	}
-}
-
-// Adds player to queue and returns matchId if
-// enough players are in queue
-func (m *Matchmaker) Add(socket *Socket) string {
-	m.elements[socket] = m.queue.PushBack(socket)
-
-	if m.queue.Len() == m.players {
-		players := make([]*Socket, 0)
-		for i := 0; i < m.players; i++ {
-			player := m.queue.Remove(m.queue.Front()).(*Socket)
-			delete(m.elements, player)
-			players = append(players, player)
-		}
-
-		matchId := m.createMatch(players)
-		for _, player := range players {
-			if player != socket {
-				go player.Send(matchId)
-			}
-		}
-
-		return matchId
-	}
-
-	return ""
-}
-
-// Remove a player from queue and return
-// whether it was successful
-func (m *Matchmaker) Remove(socket *Socket) bool {
-	element, ok := m.elements[socket]
-	if ok {
-		m.queue.Remove(element)
-	}
-	return ok
 }
 
 func (m *Matchmaker) Accept(socket *Socket, matchId string) string {
@@ -96,7 +50,7 @@ func (m *Matchmaker) Accept(socket *Socket, matchId string) string {
 	return ""
 }
 
-func (m *Matchmaker) createMatch(players []*Socket) string {
+func (m *Matchmaker) CreateMatch(players []*Socket) string {
 	match := &Match{
 		ID:      uuid.New(),
 		players: players,
--- pkg/matchmaker_test.go
+++ pkg/matchmaker_test.go
@@ -7,71 +7,13 @@ import (
 )
 
 func TestMatchmaker(t *testing.T) {
-	t.Run("enqueue", func(t *testing.T) {
-		matchmaker := pkg.NewMatchmaker(2)
-
-		socket := pkg.NewSocket()
-		res := matchmaker.Add(socket)
-
-		if res != "" {
-			t.Error("Should not get a response")
-		}
-	})
-
-	t.Run("match found", func(t *testing.T) {
-		matchmaker := pkg.NewMatchmaker(2)
-
-		p1 := pkg.NewSocket()
-		p2 := pkg.NewSocket()
-
-		matchmaker.Add(p1)
-		matchId := matchmaker.Add(p2)
-
-		if matchId == "" {
-			t.Fatal("Expected match ID, got nothing")
-		}
-
-		if res := <-p1.Outgoing; res != matchId {
-			t.Errorf("Expected %v, got %v", matchId, res)
-		}
-	})
-
-	t.Run("dequeue", func(t *testing.T) {
-		matchmaker := pkg.NewMatchmaker(4)
-
-		socket := pkg.NewSocket()
-		matchmaker.Add(socket)
-
-		if !matchmaker.Remove(socket) {
-			t.Error("Should remove from queue")
-		}
-	})
-
-	t.Run("cleanup when match found", func(t *testing.T) {
-		matchmaker := pkg.NewMatchmaker(2)
-
-		p1 := pkg.NewSocket()
-		p2 := pkg.NewSocket()
-
-		matchmaker.Add(p1)
-		matchmaker.Add(p2)
-
-		if matchmaker.Remove(p1) {
-			t.Error("Expected p1 to not be in queue anymore")
-		}
-		if matchmaker.Remove(p2) {
-			t.Error("Expected p2 to not be in queue anymore")
-		}
-	})
-
 	t.Run("accept match", func(t *testing.T) {
 		matchmaker := pkg.NewMatchmaker(2)
 
 		p1 := pkg.NewSocket()
 		p2 := pkg.NewSocket()
 
-		matchmaker.Add(p1)
-		matchId := matchmaker.Add(p2)
+		matchId := matchmaker.CreateMatch([]*pkg.Socket{p1, p2})
 
 		if res := matchmaker.Accept(p1, matchId); res != "" {
 			t.Errorf("Expected no response, got %v", res)
