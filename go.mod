56828d845e4cde99a2fdaccb81e5c9e2f9aafc56
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-10-23 16:31:32 -0300

    refactor: socket

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module git.internal.com/wingspan
 
 go 1.19
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/google/uuid v1.3.0
+	github.com/gorilla/websocket v1.5.0
+)
--- pkg/socket.go
+++ pkg/socket.go
@@ -1,15 +1,84 @@
 package pkg
 
+import (
+	"bytes"
+	"encoding/gob"
+	"io"
+	"io/ioutil"
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+
 type Socket struct {
-	Outgoing chan any
+	conn     *websocket.Conn
+	Incoming chan Message
+	Outgoing chan Response
 }
 
-func NewSocket() *Socket {
-	return &Socket{
-		Outgoing: make(chan any),
+func NewTestSocket() io.ReadWriter {
+	return &bytes.Buffer{}
+}
+
+func NewSocket(conn *websocket.Conn) *Socket {
+	socket := &Socket{
+		conn:     conn,
+		Incoming: make(chan Message),
+		Outgoing: make(chan Response),
 	}
+
+	go func() {
+		for {
+			data, err := ioutil.ReadAll(socket)
+			if err != nil {
+				log.Printf("Could not read message: %v", err)
+				continue
+			}
+
+			buf := bytes.NewBuffer(data)
+			decoder := gob.NewDecoder(buf)
+
+			var message Message
+			if err := decoder.Decode(&message); err != nil {
+				log.Printf("Could not decode message: %v", message)
+				continue
+			}
+			socket.Incoming <- message
+		}
+	}()
+
+	go func() {
+		for {
+			response := <-socket.Outgoing
+
+			buf := bytes.Buffer{}
+			encoder := gob.NewEncoder(&buf)
+
+			if err := encoder.Encode(response); err != nil {
+				log.Printf("Could not encode response: %v", response)
+				continue
+			}
+
+			if _, err := socket.Write(buf.Bytes()); err != nil {
+				log.Printf("Could not write response: %v", response)
+				continue
+			}
+		}
+	}()
+
+	return socket
 }
 
-func (s *Socket) Send(data any) {
-	s.Outgoing <- data
+func (s *Socket) Write(data []byte) (int, error) {
+	if err := s.conn.WriteJSON(data); err != nil {
+		return 0, err
+	}
+	return len(data), nil
+}
+
+func (s *Socket) Read(data []byte) (int, error) {
+	if err := s.conn.ReadJSON(&data); err != nil {
+		return 0, err
+	}
+	return len(data), nil
 }
