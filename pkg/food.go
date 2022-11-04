3c046b1eb9a63c0c61059b63172eec861b3d2832
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-11-03 22:17:00 -0300

    feat: birdfeeder

--- pkg/food.go
+++ pkg/food.go
@@ -1,5 +1,11 @@
 package pkg
 
+import (
+	"math/rand"
+	"sync"
+	"sync/atomic"
+)
+
 type FoodType int
 
 const FOOD_TYPE_COUNT = 5
@@ -11,3 +17,52 @@ const (
 	Fish
 	Rodent
 )
+
+type Birdfeeder struct {
+	food *sync.Map
+	size int32
+	len  int32
+}
+
+func NewBirdfeeder(size int) *Birdfeeder {
+	feeder := &Birdfeeder{
+		size: int32(size),
+		food: new(sync.Map),
+	}
+	feeder.Refill()
+	return feeder
+}
+
+func (f *Birdfeeder) GetFood(foodType FoodType) error {
+	value, ok := f.food.Load(foodType)
+	if !ok {
+		return ErrFoodNotFound
+	}
+
+	if value.(int) < 1 {
+		return ErrNotEnoughFood
+	}
+
+	atomic.AddInt32(&f.len, -1)
+	if value.(int)-1 <= 0 {
+		f.food.Delete(foodType)
+	} else {
+		f.food.Store(foodType, 1-value.(int))
+	}
+
+	return nil
+}
+
+func (f *Birdfeeder) Refill() {
+	size := atomic.LoadInt32(&f.size)
+	curr := atomic.LoadInt32(&f.len)
+
+	for i := 0; i < int(size-curr); i++ {
+		f.food.Store(FoodType(rand.Intn(FOOD_TYPE_COUNT)), 1)
+	}
+	atomic.StoreInt32(&f.len, size)
+}
+
+func (f *Birdfeeder) Len() int {
+	return int(atomic.LoadInt32(&f.len))
+}
---
+++ pkg/food_test.go
@@ -0,0 +1,36 @@
+package pkg_test
+
+import (
+	"testing"
+
+	"git.internal.com/wingspan/pkg"
+)
+
+func TestBirdfeeder(t *testing.T) {
+	t.Run("get food", func(t *testing.T) {
+		feeder := pkg.NewBirdfeeder(5)
+		err := feeder.GetFood(pkg.Seed)
+
+		if err != nil {
+			t.Fatalf("expected no error, got \"%v\"", err)
+		}
+		if feeder.Len() != 4 {
+			t.Errorf("expected len %v, got %v", 4, feeder.Len())
+		}
+		if err := feeder.GetFood(pkg.Seed); err == nil {
+			t.Error("should not have seed again")
+		}
+	})
+
+	t.Run("refill", func(t *testing.T) {
+		feeder := pkg.NewBirdfeeder(1)
+		if err := feeder.GetFood(pkg.Fish); err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		feeder.Refill()
+		if feeder.Len() != 1 {
+			t.Errorf("expected len %v, got %v", 1, feeder.Len())
+		}
+	})
+}
